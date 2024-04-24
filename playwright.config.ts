52afae028f599939b7237974974bc0d371babb53
Fathima Jamal-Deen <fathima.jamal-deen@nfocus.co.uk>
2024-04-24 10:19:33 +0100

    Editing the config file to take in every browser

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.edgewordstraining.co.uk',
-    //headless: process.env.CI ? true : false, //in the pipeline I dont want head on but if its locally then I want head on
+    headless: process.env.CI ? true : false, //in the pipeline I dont want head on but if its locally then I want head on
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
